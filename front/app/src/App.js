3f9181e6d685eca4a85b259aabc4f54afbdd78c0
Sebastien Lacour <sebastien.lacour1994@gmail.com>
2023-06-29 18:55:21 +0200

    Ajout du header, footer et du background

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className="background">
+        <main>
+          <h1>Ca marche</h1>
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 }
