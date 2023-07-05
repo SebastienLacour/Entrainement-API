ae43dd573d556f1bc7378390c1cba940a99cf57f
Sebastien Lacour <sebastien.lacour1994@gmail.com>
2023-07-05 18:07:51 +0200

    Ajout de la page pour les classes

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route,} from 'react-router-dom';
 import Home from './components/Home/Home';
 import Stats from './components/Stats/Stats';
 import Damages from './components/Damages/Damages';
+import ItemList from './components/ItemList/ItemList';
 
 function App() {
 
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/statistiques' element={<Stats/>}/>
-        <Route path='dégâts' element={<Damages/>}/>
+        <Route path='/dégâts' element={<Damages/>}/>
+        <Route path='/classes' element={<ItemList url="http://localhost:3000/api/classes/" title="classes"/>}/>
       </Routes>
     </div>
   );
---
+++ front/app/src/components/ItemList/ItemList.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import './ItemList.css'
+
+export default function ItemList(props) {
+
+    const url = props.url
+    console.log(url)
+
+    const [data, setData] = useState([])
+
+    const fetchData = () => {
+        fetch(url)
+            .then(response => response.json())
+            .then(result => setData(result))
+            .catch(error => console.error(error))
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+
+
+    return (
+        <div>
+            <Header />
+            <div className="background">
+                <div className="overlay">
+                    <main className='main-itemList'>
+                        <div className="title">
+                            <h1>{props.title}</h1>
+                            <p>Vous trouverez ici toutes les informations concernant les classes</p>
+                        </div>
+                        <div className="container">
+                            {data.map((el) => (
+                                <div className='container__item'>
+                                    <Link to={'/' + el._id} className='link'>
+                                        <div className='item__inside'>
+                                            <div className='item__head'>
+                                                <h2>{el.name}</h2>
+                                            </div>
+                                            <div className="item__body">
+                                                <img src={el.imageUrl} alt={el.name} className='item__img' />
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </div>
+                            ))}
+                        </div>
+                    </main>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
