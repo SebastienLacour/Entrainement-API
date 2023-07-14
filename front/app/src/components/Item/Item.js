1fbb3e98b66ddb7fcc7a3471fd422db09ed1527f
Sebastien Lacour <sebastien.lacour1994@gmail.com>
2023-07-14 19:20:46 +0200

    Amélioration de la page item

--- front/app/src/components/Item/Item.js
+++ front/app/src/components/Item/Item.js
@@ -28,10 +28,10 @@ export default function Item(props) {
   console.log(data)
 
   const weapons = data.equipement.armes
-  console.log(weapons)
-
   const armors = data.equipement.armure
-  console.log(armors)
+
+  const stats = data.stats
+  console.log(stats);
 
   return (
     <div>
@@ -41,52 +41,130 @@ export default function Item(props) {
           <main className='item__main'>
             <Link to={props.back} className='back'>retour</Link>
             <div className="item__container">
+
               <div className="item__title">
                 <h1>{data.name}</h1>
               </div>
+
               <div className="item__body">
+
                 <div className="item__img">
                   <img src={data.imageUrl} alt={data.name} />
                 </div>
+
+                {data.stats ? 
+                  <div className='stats'>
+                    <table>
+                      <td className = 'stats__cell'>
+                      <th className='stats__cell__head'>
+                        niveau
+                      </th>
+                      <tr className = 'stats__cell__row'>
+                        {data.stats.niveau}
+                      </tr>
+                      </td>
+                      <td className = 'stats__cell'>
+                      <th className='stats__cell__head'>
+                        vigueur
+                      </th>
+                      <tr className = 'stats__cell__row'>
+                        {data.stats.vigeur}
+                      </tr>
+                      </td>
+                      <td className = 'stats__cell'>
+                      <th className='stats__cell__head'>
+                        esprit
+                      </th>
+                      <tr className = 'stats__cell__row'>
+                        {data.stats.esprit}
+                      </tr>
+                      </td>
+                      <td className = 'stats__cell'>
+                      <th className='stats__cell__head'>
+                        endurance
+                      </th>
+                      <tr className = 'stats__cell__row'>
+                        {data.stats.endurance}
+                      </tr>
+                      </td>
+                      <td className = 'stats__cell'>
+                      <th className='stats__cell__head'>
+                        force
+                      </th>
+                      <tr className = 'stats__cell__row'>
+                        {data.stats.force}
+                      </tr>
+                      </td>
+                      <td className = 'stats__cell'>
+                      <th className='stats__cell__head'>
+                        dextérité
+                      </th>
+                      <tr className = 'stats__cell__row'>
+                        {data.stats.dextérité}
+                      </tr>
+                      </td>
+                      <td className = 'stats__cell'>
+                      <th className='stats__cell__head'>
+                        intelligence
+                      </th>
+                      <tr className = 'stats__cell__row'>
+                        {data.stats.intelligence}
+                      </tr>
+                      </td>
+                      <td className = 'stats__cell'>
+                      <th className='stats__cell__head'>
+                        foi
+                      </th>
+                      <tr className = 'stats__cell__row'>
+                        {data.stats.foi}
+                      </tr>
+                      </td>
+                      <td className = 'stats__cell'>
+                      <th className='stats__cell__head'>
+                        ésotérisme
+                      </th>
+                      <tr className = 'stats__cell__row'>
+                        {data.stats.ésotérisme}
+                      </tr>
+                      </td>
+                    </table>
+                  </div>
+                  :
+                  <div>
+
+                  </div>
+                }
+                {data.equipement ?
+                  <div className="item__equipement">
+                    <h2>équipements</h2>
+                    <div className='item__equipement__container'>
+                      <div className='item__equipement__list'>
+                        <h3>armes</h3>
+                        <ul>
+                          {weapons.map((el) => (
+                            <li>{el}</li>
+                          ))}
+                        </ul>
+                      </div>
+                      <div className='item__equipement__list'>
+                        <h3>armures</h3>
+                        <ul>
+                          {armors.map((el) => (
+                            <li>{el}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  :
+                  <div>
+
+                  </div>
+                }
                 <div className="item__description">
                   <cite>{data.description}</cite>
                 </div>
-                <div className="item__equipement">
-                  <h2>équipements</h2>
-                  <div className='item__equipement__container'>
-                    <div className='item__equipement__list'>
-                      <h3>armes</h3>
-                      <ul>
-                        {weapons.map((el) => (
-                          <li>{el}</li>
-                        ))}
-                      </ul>
-                    </div>
-                    <div className='item__equipement__list'>
-                      <h3>armures</h3>
-                      <ul>
-                        {armors.map((el) => (
-                          <li>{el}</li>
-                        ))}
-                      </ul>
-                    </div>
-                  </div>
-                </div>
               </div>
-              {/* <div className="item__title">
-                <h1>{item.name}</h1>
-              </div>
-              <div className="item__body">
-                <div className="item__img">
-                  <img src={item.imageUrl} alt={item.name}/>
-                </div>
-                <div className="item__description">
-                  <cite>{item.description}</cite>
-                </div>
-                <div className="item__equipement">
-
-                </div>
-              </div> */}
             </div>
           </main>
         </div>
